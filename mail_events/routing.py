f917a772f9f0c73da027df5f43130239ca03fbb9
morteza <mhoushangi1998@gmail.com>
2023-03-09 21:13:33 +0330

    change websocket naming

--- mail_events/routing.py
+++ mail_events/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/chat/$", consumers.MandrillEventsConsumer.as_asgi()),
+    re_path(r"ws/event/$", consumers.MandrillEventsConsumer.as_asgi()),
 ]
